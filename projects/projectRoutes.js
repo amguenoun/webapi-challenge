61952b12ea27a35ef85cfa8affaf8a7ab4a53f0b
Ahmad Guenoun <amguenoun@gmail.com>
2019-11-15 18:40:39 -0500

    created middleware to validate ids and projects

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -8,35 +8,59 @@ router.get('/', (req, res) => {
         .catch(err => res.status(500).json({ message: 'Could not get projects from the database' }));
 });
 
-router.get('/:id', (req, res) => {
-    db.get(req.params.id)
-        .then(data => res.status(200).json(data))
-        .catch(err => res.status(500).json({ message: 'Could not get project from the database' }));
+router.get('/:id', validateProjectId, (req, res) => {
+    res.status(200).json(req.project);
 });
 
-router.get('/:id/actions', (req, res) => {
+router.get('/:id/actions', validateProjectId, (req, res) => {
     db.getProjectActions(req.params.id)
         .then(data => res.status(200).json(data))
         .catch(err => res.status(500).json({ message: 'Could not get project actions from the database' }));
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', validProject, (req, res) => {
     db.insert(req.body)
         .then(data => res.status(200).json(data))
         .catch(err => res.status(500).json({ message: 'Could not create project' }));
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateProjectId, (req, res) => {
     db.update(req.params.id, req.body)
         .then(data => res.status(200).json(data))
         .catch(err => res.status(500).json({ message: 'Could not update project' }));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateProjectId, (req, res) => {
     db.remove(req.params.id)
         .then(data => res.status(200).json({ message: "Project successfully deleted" }))
         .catch(err => res.status(500).json({ message: 'Could not delete project' }));
 });
+
+function validateProjectId(req, res, next) {
+    db.get(req.params.id)
+        .then(project => {
+            if (!project) {
+                res.status(404).json({ message: 'Project Id Does not Exist' })
+            } else {
+                req.project = project;
+                next();
+            }
+        })
+        .catch(err => res.status(500).json({ message: 'Could not get project from the database' }));
+}
+
+function validProject(req, res, next) {
+    const project = req.body;
+    if (!project.name) {
+        res.status(400).json({ message: 'missing required name field' });
+    }
+    else if (!project.description) {
+        res.status(400).json({ message: 'missing required description field' });
+    }
+    else {
+        next();
+    }
+}
 
 module.exports = router;
