0b55b1e9fee1b255727980f305763f5bc3e43347
Ahmad Guenoun <amguenoun@gmail.com>
2019-11-15 18:13:16 -0500

    set up endpoint skeleton

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -1,4 +1,29 @@
 const router = require('express').Router();
 
+const db = require('../data/helpers/projectModel');
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.get('/:id/actions', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
 
 module.exports = router;
